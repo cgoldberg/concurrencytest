931633bfc2bac65e3f30522774afc7c8a1c3a1ad
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2026-03-06 11:01:56 -0500

    Open new version for development

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="concurrencytest",
-    version="0.1.5",
+    version="0.1.6dev0",
     py_modules=["concurrencytest"],
     install_requires=["python-subunit", "testtools"],
     author="Corey Goldberg",
